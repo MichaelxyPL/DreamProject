401889bcd13ea0d8a4c5ae0d02e32a4db4e7d171
verskid <krzysztof.sepski@gmail.com>
2022-06-02 11:41:49 +0200

    Poprawienie struktury paczek part 2

---
+++ src/main/java/com/DreamProject/SQLParser.java
@@ -0,0 +1,22 @@
+package com.DreamProject;
+
+public class SQLParser {
+
+    private String query="";
+
+    public String getUser(String name, String password) {
+        query = "SELECT " +
+                    "tbuser.id as id, tbuser.name as name, tbuser.secondname as secondname" +
+                "FROM" +
+                    "tbuser" +
+                    "inner join tblogin on tbuser.id=tblogin.userid" +
+                "WHERE " +
+                    "tblogin.name="+name+" and tblogin.pswr="+password;
+        return query;
+    }
+
+    public String getUsers() {
+        query = "select id, firstname, lastname, email, reg_date, salt, password from tbusers";
+        return query;
+    }
+}
